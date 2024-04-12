14fdbb655c183d7590a879ef17884530e0a43acb
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-04-12 10:14:53 -0500

    Created Style class

---
+++ Models/Style.cs
@@ -0,0 +1,8 @@
+namespace CarBuilder.Models;
+
+public class Style
+{
+    public int Id {get; set;}
+    public string Type {get; set;} = string.Empty;
+    public decimal PriceMultiplier {get; set;}
+}
